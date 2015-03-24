657b930b6de49ece7bc8e2cbd8bb21822ffd3bca
Simon Wollwage <mail.wollwage@gmail.com>
2015-03-24 15:18:03 +0900

    updated testcases

--- core/tests/core.rs
+++ core/tests/core.rs
@@ -1,4 +1,5 @@
 use wtftw_core::core::*;
+use wtftw_core::layout::FullLayout;
 
 extern crate wtftw_core;
 
@@ -72,4 +73,15 @@ fn stack_contains() {
     assert!(!s1.contains(23));
     assert!(!s2.contains(42));
     assert!(s2.contains(23));
+}
+
+#[test]
+fn workspace_contains() {
+    let s1 = Stack::new(42, vec!(2, 3), vec!(4, 5, 6));
+    let w1 = Workspace::new(1, String::from_str("Foo"), Box::new(FullLayout), Some(s1));
+    let w2 = Workspace::new(1, String::from_str("Foo"), Box::new(FullLayout), None);
+
+    assert!(w1.contains(42));
+    assert!(!w1.contains(23));
+    assert!(!w2.contains(2));
 }
